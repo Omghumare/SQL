9508dcec23833157174dc46a69f946e644c6d478
OM GHUMARE <omghumare17@gmail.com>
2026-05-25 22:51:25 +0530

    Add files via upload

---
+++ Find duplicat.sql
@@ -0,0 +1,26 @@
+--Find duplicat from table
+create table users
+(
+user_id int primary key,
+user_name varchar(30) not null,
+email varchar(50));
+
+insert into users values
+(1, 'Sumit', 'sumit@gmail.com'),
+(2, 'Reshma', 'reshma@gmail.com'),
+(3, 'Farhana', 'farhana@gmail.com'),
+(4, 'Robin', 'robin@gmail.com'),
+(5, 'Robin', 'robin@gmail.com');
+
+select * from users;
+
+select user_id, user_name, email
+from (
+select *,
+row_number() over (partition by user_name order by user_id) as rn
+from users u
+order by user_id) x
+where x.rn <> 1;
+
+
+	
